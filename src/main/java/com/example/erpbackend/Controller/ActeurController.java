e89e3d294e5d92e387d2bc2d483afa58e24135a3
Adjaratou7894 <81575162+Adjaratou7894@users.noreply.github.com>
2022-09-22 16:42:02 +0000

    Ajout des fonctionnalites pour Acteur

--- src/main/java/com/example/erpbackend/Controller/ActeurController.java
+++ src/main/java/com/example/erpbackend/Controller/ActeurController.java
@@ -1,11 +1,35 @@
 package com.example.erpbackend.Controller;
-
+import com.example.erpbackend.Model.Acteur;
+import com.example.erpbackend.Service.ActeurService;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @Api(value = "test", description = "")
+@AllArgsConstructor
 @RequestMapping("/acteur")
 public class ActeurController {
+
+    private final ActeurService acteurService;
+    @PostMapping("/creeracteur")
+    public Acteur creerActeur( @RequestBody Acteur acteur){
+        return acteurService.creerActeur(acteur);
+    }
+    @PutMapping("/modifieracteur/{id}")
+    public Acteur modifierActeur(  @PathVariable Long id,@RequestBody Acteur acteur){
+        return acteurService.modifierActeur(id, acteur);
+    }
+    @GetMapping("/afficheracteur")
+    public List<Acteur> afficheracteur(){
+        return acteurService.afficherToutLesActeurs();
+    }
+    @DeleteMapping("/supprimer/{id}")
+    public String delete (@PathVariable Long id){
+        return acteurService.SupprimerActeur(id);
+    }
+
+
 }
--- src/main/java/com/example/erpbackend/Service/ActeurService.java
+++ src/main/java/com/example/erpbackend/Service/ActeurService.java
@@ -1,4 +1,19 @@
 package com.example.erpbackend.Service;
 
+
+import com.example.erpbackend.Model.Acteur;
+
+import java.util.List;
+
 public interface ActeurService {
+    // Création d'un acteur
+    Acteur creerActeur(Acteur acteur);
+
+    // Mise à jour d'un acteur
+    Acteur modifierActeur( Long id ,Acteur acteur);
+
+    List<Acteur> afficherToutLesActeurs();
+    String SupprimerActeur(Long id);
+
+
 }
