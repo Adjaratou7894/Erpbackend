510cb5e9b3632b61b769c4d916e8fda5e686618d
ADIAWIAKOYE <adiawiakoye.le10@gmail.com>
2022-09-23 08:32:58 +0000

    fonctionnalite de gerer l'importation des fichiers mise à jour 2eme version

--- src/main/java/com/example/erpbackend/Controller/PostulantController.java
+++ src/main/java/com/example/erpbackend/Controller/PostulantController.java
@@ -27,33 +27,53 @@ public class PostulantController {
     private final ListePostulantService listePostulantService;
     private final PostulantService postulantService;
 
-    @RequestMapping("/import/excel/{libelleliste}")
+    //Le controlleur permettant d'importer un fichier et créer automatiquement une liste des postulants
+    @RequestMapping("/import/excel/{libelleliste}")//il prend en parametre le libelle de la liste des postulant
     public String importFormExcel(@Param("file") MultipartFile file, @PathVariable  String libelleliste) {
+
+        /*
+         * MultipartFile, classe java permettant d'importer un ou plusieurs fichiers
+         */
 
         ConfigExcel importfichier = new ConfigExcel();
 
+//stockage de la liste des postulants retournée par la classe "ConfigExcel"  dans postulantList
+
         List<Postulant> postelist = importfichier.excelImport(file);
 
-
-
-        if(postelist.size()==0){
+        if(postelist.size()==0){//verifie si la liste est vide
 
             return "Fichier vide";
-        }else {
+        }else {//si la liste n'est pas vide
+
+            //itialisation de la classe postulant liste
             Liste_postulant liste_postulant = new Liste_postulant();
 
+            //Recuperation du libelle de la liste
             liste_postulant.setLibelleliste(libelleliste);
+
+            //on modifie la date de la liste en lui donnant la date actuelle
             liste_postulant.setDateliste(new Date());
 
+            //verifie si la liste existe déjà ou pas
             if(listePostulantService.trouverListePostulantParLibelle(liste_postulant.getLibelleliste()) == null){
+                //on crée la liste et garder cette listepostulant dans lpt
                 Liste_postulant lpt = listePostulantService.creerlistepostulant(liste_postulant);
+
+                /*
+                 * Dans la boucle for suivant on parcours la liste des postulants en lui ajoutant l'id de la liste
+                 * sur la quelle la liste a été tiré
+                 */
 
                 for(Postulant pot:postelist){
 
+                    //ajout de l'id de la liste à tous les  postulants
                     pot.setListePostulant(lpt);
+                    //attribuer l'etat true par defeaut au postulant tirer
                     pot.setEtat(true);
                 }
                 System.out.println(postelist);
+                //enregistrement de la liste des postulants importés dans la base
                 postulantService.enregistrerPostulant(postelist);
                 return "liste importer avec succes";
             }else {
