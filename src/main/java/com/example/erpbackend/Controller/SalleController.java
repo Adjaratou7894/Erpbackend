2f6c25d82542c9ed7fdffcf223b949c18fad3e25
adcoulibaly <coulibalyadamabekaye03@gmail.com>
2022-09-27 14:19:15 +0000

    Ajout de super Admin et les roles

--- src/main/java/com/example/erpbackend/Controller/SalleController.java
+++ src/main/java/com/example/erpbackend/Controller/SalleController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins ="http://localhost:8100")
 @Api(value = "Test", description = "controller permettent la Gestion Des Salle")
 @RequestMapping("/salle")
 @CrossOrigin(origins = "http://localhost:8100")
